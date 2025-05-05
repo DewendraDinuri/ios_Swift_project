0be6f167a35da7435a72593da9e1e1514ccb5e33
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-05-05 20:03:00 +0530

    add initial ios project

---
+++ Onboading_page/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onboading_page
+//
+//  Created by Dinuri Dewendra on 2025-05-05.
+//
+// add commit 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
