fa2073e9a962537fc66611c05cb86ca71fd0b3c1
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-05-05 22:32:50 +0530

    Update onboadingView.swift

--- Onboading_page/ContentView.swift
+++ Onboading_page/ContentView.swift
@@ -4,21 +4,10 @@
 //
 //  Created by Dinuri Dewendra on 2025-05-05.
 //
-// add commit 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        OnboardingView()
     }
-}
-
-#Preview {
-    ContentView()
 }
